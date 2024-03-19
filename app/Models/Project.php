5b15bad2ea5a9147e786dd8002a551ebe01c47d7
Nerijus <krekas@gmail.com>
2024-03-19 13:28:05 +0200

    Setting `user_id` Column Automatically: Two Ways

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -8,5 +8,13 @@ class Project extends Model
 {
     protected $fillable = [
         'name',
+        'user_id',
     ];
+
+    protected static function booted(): void
+    {
+        static::creating(function (Project $project) {
+            $project->user_id = auth()->id();
+        });
+    }
 }
